aaadfc28e8b706c6d2f9b32e16a24f2ff0a51dd9
Mehdi <mehdiougadiofficial@gmail.com>
2026-01-08 12:35:17 -0500

    [NEW] Mehdi: started compute variables .tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,3 @@
 provider "aws" {
-    "region": "ca-central-1"
+    region = "ca-central-1"
 }
-
-resource "aws_vpc" "cloud_vpc"{
-
-}
-
-resource "aws_subnet" "public_subnet"{
-    
-}
-
-resource "aws_subnet" "private_subnet"{
-    
-}
-
-resource "aws_internet_gateway" "igw"{
-    
-}
-
-resource "aws_nat_gateway" "ngw"{
-    
-}
-
-resource "aws_security_group" "sg-" {
-  
-}
-
---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,39 @@
+variable "subnet_id" {
+  description = "The ID of the VPC subnet where the EC2 instance will be launched"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The ID of the Amazon Machine Image (AMI) to use for the instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name tag to assign to the EC2 instance"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group to attach to the instance"
+  type        = string
+}
+
+variable "user_data_id" {
+  description = "The user data script or configuration to run at instance launch"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair to use for instance access"
+  type        = string
+}
+
+variable "count" {
+  description = "The number of EC2 instances to create"
+  type        = number
+}
